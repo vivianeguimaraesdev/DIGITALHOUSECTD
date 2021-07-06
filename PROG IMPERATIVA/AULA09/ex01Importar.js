0f0c1a8e8a43ebf23a9c97f1dcd8e6d1ae7e7149
Viviane Guimarães <83373229+vivianeguimaraesdev@users.noreply.github.com>
2021-07-06 16:13:24 -0300

    Create ex01Importar.js

---
+++ PROG IMPERATIVA/AULA09/ex01Importar.js
@@ -0,0 +1,8 @@
+//Aula 09 - 21/06/2021
+//Fora da pasta Modulos
+//utilizando o módulo criado na pasta
+//ex04Importar.js
+
+let cumprimento = require('./modulos/exCumprimento.js'); //requerer
+cumprimento=cumprimento+' William';
+console.log(cumprimento);
