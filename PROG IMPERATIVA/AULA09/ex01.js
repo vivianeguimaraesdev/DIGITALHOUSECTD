7e78e4c32aa4eb9bfe046237a1273feb89af5ca5
Viviane Guimarães <83373229+vivianeguimaraesdev@users.noreply.github.com>
2021-07-06 16:13:14 -0300

    Create ex01.js

---
+++ PROG IMPERATIVA/AULA09/ex01.js
@@ -0,0 +1,13 @@
+//Aula 9 - 21/06/2021
+//Objetos Literais
+
+let carro = {
+    placa: 'ADF1234',
+    cor: 'prata',
+    ano: 2021,
+    flex: true
+}
+console.log(carro.placa);
+console.log(carro.cor);
+console.log(carro.ano);
+console.log(carro.flex);
