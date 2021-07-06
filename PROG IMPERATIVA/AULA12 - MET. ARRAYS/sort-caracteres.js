9d259a94537f6e042243975049bd1460030e0c33
Viviane Guimarães <83373229+vivianeguimaraesdev@users.noreply.github.com>
2021-07-06 16:14:14 -0300

    Create sort-caracteres.js

---
+++ PROG IMPERATIVA/AULA12 - MET. ARRAYS/sort-caracteres.js
@@ -0,0 +1,8 @@
+//Aula 12 - Metodos de Arrays Avançados 
+//06/07/2021
+// Sort = ordenar
+//array.sort() nos ajuda a ordenar os elementos de um array
+
+const frutas = ['Maçã','Banana','Acerola','Laranja','Limão','Abacate'];
+frutas.sort();
+console.log(frutas);
