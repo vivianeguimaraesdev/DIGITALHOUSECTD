84b854cc621ca8ea5962260d8b3a83d81eb22497
Viviane Guimarães <83373229+vivianeguimaraesdev@users.noreply.github.com>
2021-07-06 16:12:46 -0300

    Create ex6impFuncoes.js

---
+++ PROG IMPERATIVA/AULA09/bkp/ex6impFuncoes.js
@@ -0,0 +1,8 @@
+const mais=require('./modulos/ex6expFuncoes');
+const menos=require('./modulos/ex6expFuncoes');
+const maiuscula=require('./modulos/ex6expFuncoes');
+
+console.log(mais.mais(2,3))
+console.log(menos.menos(20,3))
+maiuscula.maiuscula('william');
+mais.maiuscula('aaa');
