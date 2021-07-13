443a7b9cc376d3e5879caaf84bb980bb7ff0fe55
Viviane Guimarães <83373229+vivianeguimaraesdev@users.noreply.github.com>
2021-07-13 16:07:50 -0300

    Create ex03.js

---
+++ PROG IMPERATIVA/AULA13/ex03.js
@@ -0,0 +1,5 @@
+/* Em um arquivo diferente, crie o objeto literal 
+curso que tem como atributos: nome do curso (string), 
+nota de aprovação (number), faltas máximas (number) 
+e uma lista de estudantes (um array composto pelos alunos criados no passo 1).*/
+
