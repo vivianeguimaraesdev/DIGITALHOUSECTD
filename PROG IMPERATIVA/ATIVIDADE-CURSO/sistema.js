a8ad396e01ed3f8f368eea5b681e7ff9140eb211
Viviane Guimarães <83373229+vivianeguimaraesdev@users.noreply.github.com>
2021-07-21 18:38:40 -0300

    Update sistema.js

--- PROG IMPERATIVA/ATIVIDADE-CURSO/sistema.js
+++ PROG IMPERATIVA/ATIVIDADE-CURSO/sistema.js
@@ -5,10 +5,10 @@ let Aluno = require("./aluno");
 // Importação do arquivo curso
 let curso = require("./curso");
 
-let joao = new Aluno("Victor", 1, [9, 10, 8, 7]);
-let fernanda = new Aluno("Alana", 2, [10, 10, 8, 9]);
-let cleber = new Aluno("Lucas", 4, [6, 5, 5, 6]);
-let lirio = new Aluno("Maria", 3, [7,5,8,6]);
+let victor = new Aluno("Victor", 1, [9, 10, 8, 7]);
+let alana = new Aluno("Alana", 2, [10, 10, 8, 9]);
+let lucas = new Aluno("Lucas", 4, [6, 5, 5, 6]);
+let maria = new Aluno("Maria", 3, [7,5,8,6]);
 
 
 curso.adicionarAluno(joao, fernanda, cleber,lirio)
